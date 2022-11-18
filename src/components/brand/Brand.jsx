bbbb16f641f0cf97819234a193b6256ca46811e0
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-19 00:12:37 +0100

    done with hero

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import "./brand.css"
+import React from "react";
+import "./brand.css";
+import { google, atlassian, slack, dropbox, shopify } from "../../assets";
+
+const images = [google, atlassian, slack, dropbox, shopify];
 
 const Brand = () => {
   return (
-    <div>
-      brand
+    <div className="gpt3__brand section__padding">
+      {images.map((image, index) => (
+        <div key={index}>
+          <img src={image} alt="images" />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
